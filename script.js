9a189783b1da51d27df1cfdb486f56e8d4f38772
apsala <apsala>
2025-01-23 10:08:02 +0530

    Java Script

---
+++ script.js
@@ -0,0 +1,25 @@
+function example2(){
+    document.getElementById('ex').
+    externalHTML="External from Script";
+}
+function handleLogin()
+{
+    const username=document.getElementById('user').value ;
+    const password=document.getElementById('pass').value ;
+    const error=document.getElementById('error') 
+
+    const loginData=[
+        {username:"Apsala",password:"pass123"},
+        {username:"Abi",password:"123pass"},
+        {username:"Madhu",password:"123@4"},
+    ]
+    const isValid=loginData.some((user)=>user.username===username && user.password===password)
+    if (isValid)
+    {
+        window.location.href='flexbox.html'
+        error.innerText="login Successful";
+    }
+    else{
+        error.innerText="Invalid Credentials";
+    }
+}
